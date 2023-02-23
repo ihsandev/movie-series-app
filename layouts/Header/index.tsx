30df166678044c37cd32ce077c60c93ad56e6f4f
alihsan <ihksansanhas@gmail.com>
2023-02-23 16:25:23 +0700

    update: add link to home on title header

--- layouts/Header/index.tsx
+++ layouts/Header/index.tsx
@@ -9,9 +9,9 @@ const Header: FC<IHeader> = ({ subtitleHeader }) => (
   <header className="fixed top-0 right-0 left-0">
     <nav className="bg-blue-600 text-white py-3 px-2 md:px-0">
       <div className="container mx-auto flex justify-between items-center">
-        <div>
+        <Link href={'/'}>
           <h1 className="text-4xl">Demo Streaming</h1>
-        </div>
+        </Link>
         <div className="flex items-center">
           <Link href={'/login'} className="p-2 mr-6">Log In</Link>
           <Link href={'/trial'} className="bg-slate-900 text-white p-2">
